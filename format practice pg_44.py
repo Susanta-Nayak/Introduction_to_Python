7cf8e5386e3aaea6458c6745492a576b2373379c
antoinettehemlall <antoinette.hemlall2@gmail.com>
2020-06-06 18:52:51 -0400

    Fixed formatting issue on pg 44 file

--- format practice pg_44.py
+++ format practice pg_44.py
@@ -1,6 +1,7 @@
 hours=eval(input("Please enter the number of hours you worked:"))
 rate=eval(input("Please enter you hourly rate:"))
 total=hours*rate
-print("You earned $" + format(total,".2f"),'for',format(hours,'.1f'),'hours of work at $'+format(rate,'.2f'),'/hour.')
+print("You earned $" + format(total,".2f"),'for',format(hours,'.1f'),
+      'hours of work at $'+format(rate,'.2f')+'/hour.')
 #there are other ways to format, but the last line uses the way taught in class
-#try to find a way to remove the space between .70 /hour
+#used concatenation to remove the space between cents and /hour
